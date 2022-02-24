b1414c4b812cb5350279a6add317cfe1bcbf8260
Luan E <luan.erazo@skyrocketlab.space>
2022-02-23 21:37:04 -0500

    acomodado el g-capcha en caminos del saman, falta replicar

--- src/mainRR.js
+++ src/mainRR.js
@@ -1,5 +1,16 @@
-import {form} from './components/staticForm.js';
 
-// Carga del formulario
-const formContainer = document.getElementById("staticFormContainer");
-formContainer.append(form());
+// ------------- Tomo la respuesta del capcha para saber que ocurrio
+let recaptcha_response = "";
+function submitUserForm() {
+    if (recaptcha_response.length == 0) {
+        document.getElementById("g-recaptcha-error").innerHTML =
+            '<span style="color:red;">This field is required.</span>';
+        return false;
+    }
+    return true;
+}
+
+function verifyCaptcha(token) {
+    recaptcha_response = token;
+    document.getElementById("g-recaptcha-error").innerHTML = "";
+}
---
+++ src/mainRRModules.js
@@ -0,0 +1,6 @@
+import { form } from './components/staticForm.js';
+
+// Carga del formulario
+const formContainer = document.getElementById("staticFormContainer");
+
+formContainer.append(form());
