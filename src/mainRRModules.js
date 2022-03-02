d207cc9b664cdc5d4dced1c7c903027dcf348980
Luan E <luan.erazo@skyrocketlab.space>
2022-03-02 16:36:03 -0500

    creacion del header, sin embargo no funciono como se esperaba

--- src/mainRRModules.js
+++ src/mainRRModules.js
@@ -1,27 +1,30 @@
 import { form } from './components/staticForm.js';
 import { form as popForm } from './components/popForm.js';
 
-
-// Carga del formulario estatico
-const formContainer = document.getElementById("staticFormContainer");
-formContainer.append(form());
-
-// Carga del formulario estatico
-const popFormContainer = document.getElementById("popFormContainer");
-popFormContainer.append(popForm());
-// Tomamos el name del padre
-const projectName = formContainer.getAttribute('name');
-formContainer
-  .querySelector("#cf-proyecto")
-  .setAttribute(
-    "value",
-    projectName);
-
-popFormContainer
-  .querySelector("#cf-proyecto-pop")
-  .setAttribute(
-    "value",
-    projectName);
+try {
+  // Carga del formulario estatico
+  const formContainer = document.getElementById("staticFormContainer");
+  formContainer.append(form());
+  
+  // Carga del formulario estatico
+  const popFormContainer = document.getElementById("popFormContainer");
+  popFormContainer.append(popForm());
+  // Tomamos el name del padre
+  const projectName = formContainer.getAttribute('name');
+  formContainer
+    .querySelector("#cf-proyecto")
+    .setAttribute(
+      "value",
+      projectName);
+  
+  popFormContainer
+    .querySelector("#cf-proyecto-pop")
+    .setAttribute(
+      "value",
+      projectName);
+} catch (error) {
+  console.error('NO se encontraron formularios a los que agregar el contenedor');
+}
 console.log(formContainer.attributes);
 console.log(projectName);
-// formContainer.setAttribute()
+
